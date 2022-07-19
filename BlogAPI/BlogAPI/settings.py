e18f0c817f6865e5cc562b726ecf5008fd1e9c5b
Abdelrahman Torky <abdelrahman.torky@itworx.com>
2022-07-19 07:52:09 +0200

    Changed DB Engine

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'BlogAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BlogDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
