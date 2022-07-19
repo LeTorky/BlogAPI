d1475c273a3933c55f92333d32f10ee88a4c086b
Abdelrahman Torky <abdelrahman.torky@itworx.com>
2022-07-19 07:51:34 +0200

    Added Models

--- BlogAPI/BlogAPI/settings.py
+++ BlogAPI/BlogAPI/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'BlogAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'BlogDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 
--- BlogAPI/MainApp/models.py
+++ BlogAPI/MainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author')
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=100)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True)
+    creation_date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.title
