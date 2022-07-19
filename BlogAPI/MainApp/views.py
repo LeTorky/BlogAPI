85c6d32df2fe867a427f55241ca7e49a39b3db9a
Abdelrahman Torky <abdelrahman.torky@itworx.com>
2022-07-19 08:30:43 +0200

    Added Get Posts By Author Custom Action

--- BlogAPI/MainApp/views.py
+++ BlogAPI/MainApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import *
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -12,3 +14,11 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
+    
+    @action(detail=False, methods=['get'], url_path=r'byAuthor/(?P<pk>[0-9]+)')
+    def get_blogs_by_author(self, request, pk):
+        author = Author.objects.get(pk=pk)
+        posts = Post.objects.filter(author=author)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
