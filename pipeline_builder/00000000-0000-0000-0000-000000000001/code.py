3e51550987a8295448bd2fa129701901b9352353
Abdelrahman Torky <24torky@gmail.com>
2026-04-23 18:37:34 +0200

    Delete pipeline_builder directory

--- pipeline_builder/00000000-0000-0000-0000-000000000001/code.py
+++
@@ -1,9 +0,0 @@
-
-from uuid import uuid4
-import textwrap
-function_string = config["code"]
-func_name = f"__exec_{uuid4()}"
-indented = textwrap.indent(function_string, "  ")
-exec(f"async def {func_name}():\n{indented}")
-output = await func_name()
-return output
